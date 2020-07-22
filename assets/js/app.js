4b814dcdb2e7eaaf3b598d53cc941a014654e133
ajishtmohan <41186016+ajishtmohan@users.noreply.github.com>
2020-07-22 06:39:46 +0530

    UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,53 @@
 // 1. Job Controller
 var jobController = (function() {
+
+    var ClientDetails = function(clientID, clientName, clientAddress, clientCity, clientRep, clientEmail, clientPhone) {
+        this.clientID = clientID;
+        this.clientName = clientName;
+        this.clientAddress = clientAddress;
+        this.clientCity = clientCity;
+        this.clientRep = clientRep;
+        this.clientEmail = clientEmail;
+        this.clientPhone = clientPhone;
+    };
+
+    var data = {
+        clientData: [],
+    };
+
+    return {
+        addNewClient: function(clientName, clientAddress, clientCity, clientRep, clientEmail, clientPhone) {
+            var clientID = 0;
+            var newClientAdded = new ClientDetails(clientID, clientName, clientAddress, clientCity, clientRep, clientEmail, clientPhone);
+            console.log(newClientAdded);
+
+            data.clientData.push(newClientAdded);
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    }
     
 })();
 
+
+
 // 2. UI Controller
 var UIController = (function() {
+
+    var DOMstrings = {
+        // New Client Form
+        inputClientName: '#new-client-name',
+        inputClientAddress: '#new-client-address',
+        inputClientCity: '#new-client-city',
+        inputClientRep: '#new-client-rep',
+        inputClientEmail: '#new-client-email',
+        inputClientPhone: '#new-client-phone',
+
+        // Client List
+        clientContainer: '.new-clients',
+    }
 
     var hideSideMenu = function() {
         var leftMenu = document.querySelector('.left-menu');
@@ -81,39 +124,128 @@ var UIController = (function() {
         e.target.classList.add('.left-menu-active');
         }
     };
+
+    var showClientForm = function() {
+        document.querySelector('.new-client-form').style.display = 'block';
+        setTimeout(function() { 
+            document.querySelector('.new-client-form').style.opacity = '1';
+        }, 300);
+    };
+
+    var hideClientForm = function() {
+        document.querySelector('.new-client-form').style.opacity = '0';
+        setTimeout(function() { 
+            document.querySelector('.new-client-form').style.display = 'none';
+        }, 300);
+        
+    };
     
 
     return {
+        getClientData: function() {
+            return {
+                clientName: document.querySelector(DOMstrings.inputClientName).value,
+                clientAddress: document.querySelector(DOMstrings.inputClientAddress).value,
+                clientCity: document.querySelector(DOMstrings.inputClientCity).value,
+                clientRep: document.querySelector(DOMstrings.inputClientRep).value,
+                clientEmail: document.querySelector(DOMstrings.inputClientEmail).value,
+                clientPhone: document.querySelector(DOMstrings.inputClientPhone).value,
+            }
+        },
+
+        clearClientForm: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputClientName + ', ' + DOMstrings.inputClientAddress + ', ' + DOMstrings.inputClientCity + ', ' + DOMstrings.inputClientRep + ', ' + DOMstrings.inputClientEmail + ', ' + DOMstrings.inputClientPhone);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = '';
+            });
+            fieldsArr[0].focus();
+        },
+
+        addClientListItem: function() {
+            var html, newHtml, element, clientInfo;
+
+            element = DOMstrings.clientContainer;
+
+            clientInfo = [1, DOMstrings.inputClientName, DOMstrings.inputClientAddress, DOMstrings.inputClientCity, DOMstrings.inputClientRep, DOMstrings.inputClientEmail, DOMstrings.inputClientPhone]
+
+            html = `<div class="client-number"><p>No</p></div><div class="client-name"><p>${jobController.newClientAdded.clientName}</p></div><div class="client-address"><p>Address</p></div><div class="client-city"><p>City</p></div><div class="client-person-incharge"><p>clientRep</p></div><div class="client-email"><p>Email</p></div><div class="client-phone"><p>Phone</p></div>`,
+
+            console.log(html);
+
+        },
+
         showDashboard,
         showJobs,
         showClients,
         hideSideMenu,
         activateBtn,
+        showClientForm,
+        hideClientForm,
 
-        
-        
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
     }
     
 })();
 
 // 3. Central Controller
 var controller = (function(jobCtrl, UICtrl) {
-    // Hide Side Menu Bar
-    document.querySelector('.noti-bar-title').addEventListener('click', UIController.hideSideMenu);
 
-    // Show Dashboard
-    document.querySelector('.dashboard-btn').addEventListener('click', UIController.showDashboard);
+    var setupEventlisteners = function() {
+        // Hide Side Menu Bar
+        document.querySelector('.noti-bar-title').addEventListener('click', UICtrl.hideSideMenu);
 
-    // Show Jobs Panel
-    document.querySelector('.jobs-btn').addEventListener('click', UIController.showJobs);
+        // Show Dashboard
+        document.querySelector('.dashboard-btn').addEventListener('click', UICtrl.showDashboard);
 
-    // Show Clients Panel
-    document.querySelector('.clients-btn').addEventListener('click', UIController.showClients);
+        // Show Jobs Panel
+        document.querySelector('.jobs-btn').addEventListener('click', UICtrl.showJobs);
+
+        // Show Clients Panel
+        document.querySelector('.clients-btn').addEventListener('click', UICtrl.showClients);
+
+        // Show New Client Form
+        document.querySelector('.add-client-btn').addEventListener('click', UICtrl.showClientForm);
+
+        // Hide New Client Form
+        document.querySelector('.client-form-cancel').addEventListener('click', UICtrl.hideClientForm);
+
+        // Add New Client
+        document.querySelector('.client-form-submit').addEventListener('click', ctrlAddClient);
+    }
+    
+
+    var ctrlAddClient = function() {
+        var inputCL, newClient;
+
+        // 1. Get Client Data from UI
+        inputCL = UICtrl.getClientData();
+        console.log(inputCL);
+
+        // 2. Add the client data to 'data'
+        newClient = jobCtrl.addNewClient(inputCL.clientName, inputCL.clientAddress, inputCL.clientCity, inputCL.clientRep, inputCL.clientEmail, inputCL.clientPhone);
+
+        UICtrl.clearClientForm();
+        UICtrl.hideClientForm();
+        
+
+        // 3. Update the Client UI
+        UICtrl.addClientListItem();
+
+    };
+
 
     return {
         init: function() {
             UICtrl.showClients()
             UICtrl.activateBtn()
+            console.log('App initialised!')
+            setupEventlisteners();
         }
         
     }
